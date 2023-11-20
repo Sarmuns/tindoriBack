a7232a459f24d4dc75c09960d199aa7ac23f9d80
Alison Brito <alisonofx@gmail.com>
2023-11-20 14:19:59 -0300

    Mensagens - Falta validacao

---
+++ controllers/MensagemController.js
@@ -0,0 +1,66 @@
+const MensagemService = require("../services/MensagemService");
+
+const MensagemController = {
+  async listarMensagens(req, res) {
+    try {
+      const { id } = req.params;
+      const { data, error } = await MensagemService.listarMensagens(id);
+
+      if (error) throw new Error(error);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  async criarMensagem(req, res) {
+    try {
+      const { conversaId, senderId, senderType, messageText } = req.body;
+      const { data, error } = await MensagemService.criarMensagem(
+        conversaId,
+        senderId,
+        senderType,
+        messageText
+      );
+
+      if (error) throw new Error(error);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  async editarMensagem(req, res) {
+    try {
+      const { id } = req.params;
+      const novosDados = req.body;
+      const { data, error } = await MensagemService.editarMensagem(
+        id,
+        novosDados
+      );
+
+      if (error) throw new Error(error);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  async deletarMensagem(req, res) {
+    try {
+      const { id } = req.params;
+      const { data, error } = await MensagemService.deletarMensagem(id);
+
+      if (error) throw new Error(error);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+};
+
+module.exports = MensagemController;
