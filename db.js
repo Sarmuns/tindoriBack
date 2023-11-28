995b4beb14720be17dd6118f9f3011f4d0f79ff4
Samuel Alcântara <samuel.afrocha@gmail.com>
2023-11-28 15:49:21 -0300

    ajeitando o erro

--- db.js
+++ db.js
@@ -2,5 +2,6 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://sfslqomvcxzpfgipcfpg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmc2xxb212Y3h6cGZnaXBjZnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1OTg5NDUsImV4cCI6MjAxNDE3NDk0NX0.bhooTIhrpBtXbV_2nX75tAEvX1wwkSeeYV7uj71hxJA';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
